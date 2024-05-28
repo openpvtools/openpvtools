01386dc0da9f01840336791717d4bdeefcbb32c6
Kevin Anderson <kevin.anderso@gmail.com>
2024-05-28 09:24:56 -0400

    fix sphinx conf.py incompatibilities

--- conf.py
+++ conf.py
@@ -60,7 +60,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -108,7 +108,7 @@ htmlhelp_basename = 'openpvtoolsdoc'
 
 
 def setup(app):
-    app.add_stylesheet('theme_override.css')
+    app.add_css_file('theme_override.css')
 
 
 # -- Options for LaTeX output ------------------------------------------------
